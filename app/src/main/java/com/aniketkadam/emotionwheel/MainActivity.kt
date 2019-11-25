9f111401041c434164a639411149c38eea4d41f7
Aniket Kadam <anikadamg@gmail.com>
2019-11-25 10:01:16 +0530

    Planting timber in debug

--- app/src/main/java/com/aniketkadam/emotionwheel/MainActivity.kt
+++ app/src/main/java/com/aniketkadam/emotionwheel/MainActivity.kt
@@ -2,11 +2,13 @@ package com.aniketkadam.emotionwheel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Timber.plant(Timber.DebugTree())
     }
 }
