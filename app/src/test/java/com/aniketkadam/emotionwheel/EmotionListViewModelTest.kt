28a61450948490267097922e7c48c05453aeac85
Aniket Kadam <anikadamg@gmail.com>
2019-11-28 11:35:36 +0530

    Added test helper libs, and tests for initial state of vm. This required updating the min sdk but we'll change this to be lowered only for tests later.

---
+++ app/src/test/java/com/aniketkadam/emotionwheel/EmotionListViewModelTest.kt
@@ -0,0 +1,36 @@
+package com.aniketkadam.emotionwheel
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.google.gson.Gson
+import com.jraska.livedata.test
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class EmotionListViewModelTest {
+
+    @get:Rule
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    lateinit var vm: EmotionListViewModel
+    val repo: IEmotionRepo = object : IEmotionRepo {
+        override val allEmotionList: Emotion
+            get() = getTextInFile("emotion_db.json").let {
+                Gson().fromJson(
+                    it,
+                    Emotion::class.java
+                )
+            }
+
+    }
+
+    @Before
+    fun setup() {
+        vm = EmotionListViewModel(repo)
+    }
+
+    @Test
+    fun `what`() {
+        vm.viewState.test().assertValue { it.currentEmotion.name == "all" }
+    }
+}
