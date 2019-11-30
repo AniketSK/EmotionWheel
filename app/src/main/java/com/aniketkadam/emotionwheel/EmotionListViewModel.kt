bc0522c397ff095eeefe3476a48db0772f54335d
Aniket Kadam <anikadamg@gmail.com>
2019-11-30 17:15:54 +0530

    ViewModel passes additional data for header list correctly

--- app/src/main/java/com/aniketkadam/emotionwheel/EmotionListViewModel.kt
+++ app/src/main/java/com/aniketkadam/emotionwheel/EmotionListViewModel.kt
@@ -15,30 +15,44 @@ class EmotionListViewModel(repo: IEmotionRepo) : ViewModel() {
         get() = _viewState
 
     init {
-        _viewState.postValue(ViewState(repo.allEmotionList))
+        _viewState.postValue(ViewState(repo.allEmotionList, listOf("All")))
     }
 
     fun onListItemClicked(clickedEmotion: Emotion?) {
         clickedEmotion?.let {
             navigationStack.push(viewState.value!!.currentEmotion)
-            _viewState.postValue(ViewState(it))
+            _viewState.postValue(ViewState(it, getCurrentHeaderList().plus(it.name)))
         }
     }
 
+    private fun getCurrentHeaderList() =
+        _viewState.value!!.headerList
+
+
     fun backPressed() {
         if (!navigationStack.isEmpty()) {
-            _viewState.postValue(ViewState(navigationStack.pop()))
+            _viewState.postValue(
+                ViewState(
+                    navigationStack.pop(),
+                    getCurrentHeaderList().dropLast(1)
+                )
+            )
         }
     }
 
     fun headerIndexClicked(position: Int) {
         if (position != 0) {
             repeat(position - 1) { navigationStack.pop() }
-            _viewState.postValue(ViewState(navigationStack.pop()))
+            _viewState.postValue(
+                ViewState(
+                    navigationStack.pop(),
+                    getCurrentHeaderList().filterIndexed { index, _ -> index <= position })
+            )
         }
     }
 }
 
 data class ViewState(
-    val currentEmotion: Emotion
+    val currentEmotion: Emotion,
+    val headerList: List<String>
 )
