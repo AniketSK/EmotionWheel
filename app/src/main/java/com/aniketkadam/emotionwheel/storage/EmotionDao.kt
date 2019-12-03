edbd18e5bd4cf8a01f52a27f9875e2274c1e3d3f
Aniket Kadam <anikadamg@gmail.com>
2019-12-03 23:15:15 +0530

    Made the insert call async

--- app/src/main/java/com/aniketkadam/emotionwheel/storage/EmotionDao.kt
+++ app/src/main/java/com/aniketkadam/emotionwheel/storage/EmotionDao.kt
@@ -5,12 +5,13 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
 import com.aniketkadam.emotionwheel.data.EmotionJourney
+import io.reactivex.Completable
 
 @Dao
 interface EmotionDao {
 
     @Insert
-    fun insert(emotionJourney: EmotionJourney)
+    fun insert(emotionJourney: EmotionJourney): Completable
 
     @Query("SELECT * from EmotionJourney")
     fun getAllJourneys(): LiveData<List<EmotionJourney>>
