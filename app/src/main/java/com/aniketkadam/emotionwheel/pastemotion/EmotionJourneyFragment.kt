a234ebb849d9946996f74cb639efbb25578a7dec
Aniket Kadam <anikadamg@gmail.com>
2019-12-06 23:07:39 +0530

    Completed injecting the journey vm and corrected comments

--- app/src/main/java/com/aniketkadam/emotionwheel/pastemotion/EmotionJourneyFragment.kt
+++ app/src/main/java/com/aniketkadam/emotionwheel/pastemotion/EmotionJourneyFragment.kt
@@ -1,15 +1,14 @@
 package com.aniketkadam.emotionwheel.pastemotion
 
+import android.content.Context
 import androidx.fragment.app.ListFragment
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.HasAndroidInjector
+import dagger.android.support.AndroidSupportInjection
 import javax.inject.Inject
 
 /**
- * A fragment representing a list of Items.
- * Activities containing this fragment MUST implement the
- * [EmotionJourneyFragment.OnListFragmentInteractionListener] interface.
  */
 class EmotionJourneyFragment : ListFragment(), HasAndroidInjector {
 
@@ -18,5 +17,12 @@ class EmotionJourneyFragment : ListFragment(), HasAndroidInjector {
 
     override fun androidInjector(): AndroidInjector<Any> = androidInjector
 
+    @Inject
+    lateinit var emotionJourneyVm: EmotionJourneyVm
+
+    override fun onAttach(context: Context) {
+        AndroidSupportInjection.inject(this)
+        super.onAttach(context)
+    }
 
 }
