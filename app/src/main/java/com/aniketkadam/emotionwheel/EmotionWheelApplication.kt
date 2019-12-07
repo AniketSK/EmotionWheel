a8e22c48121c4ed32cbd98817152b7f049e52dd5
Aniket Kadam <anikadamg@gmail.com>
2019-12-07 11:21:48 +0530

    Added jodatime for time formatting

--- app/src/main/java/com/aniketkadam/emotionwheel/EmotionWheelApplication.kt
+++ app/src/main/java/com/aniketkadam/emotionwheel/EmotionWheelApplication.kt
@@ -3,6 +3,7 @@ package com.aniketkadam.emotionwheel
 import com.aniketkadam.emotionwheel.di.DaggerAppComponent
 import dagger.android.AndroidInjector
 import dagger.android.support.DaggerApplication
+import net.danlew.android.joda.JodaTimeAndroid
 import timber.log.Timber
 
 class EmotionWheelApplication : DaggerApplication() {
@@ -17,6 +18,7 @@ class EmotionWheelApplication : DaggerApplication() {
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
         }
+        JodaTimeAndroid.init(this)
         super.onCreate()
     }
 }
